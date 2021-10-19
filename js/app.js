2bf77ba83acb3dfd7fec75fd2976a66f4dfd0c85
Anuar Kairulla <anuarkairulla@gmail.com>
2021-10-19 23:08:37 +0600

    added functional features

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 const TXTLoader = document.querySelector('#txtLoader')
 const downloadBtn = document.querySelector('#download')
 
+const createXMLDocument = () => {
+    let XMLDocument = document.implementation.createDocument(null, 'books');
+    const XMLString = "<root></root>"
+    const parser = new DOMParser()
+    XMLDocument = parser.parseFromString('<?xml version="1.0" encoding="utf-8"?><root></root>', "application/xml");
+    const root = XMLDocument.getElementsByTagName("root")[0]
+    let jobs = XMLDocument.createElement("jobs")
+    root.appendChild(jobs)
+}
+
 TXTLoader.addEventListener('change', function (e) {
     const files = TXTLoader.files
     const file = files[0]
     const convertToXML = () => {
         let reader = new FileReader()
         reader.readAsText(file);
-        reader.onload = () => { const shit = reader.result }
+        reader.onload = () => {
+            const text = reader.result
+            createXMLDocument()
+        }
     }
 
     if (files.length === 1 && file.type === 'text/plain') {
